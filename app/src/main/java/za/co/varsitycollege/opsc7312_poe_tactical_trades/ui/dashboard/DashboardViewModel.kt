54635bd31f8a8d4e4521621f8d8a8154116221d8
Mike_Turner <st10150631@vcconnect.edu.za>
2024-09-16 15:35:17 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/opsc7312_poe_tactical_trades/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package za.co.varsitycollege.opsc7312_poe_tactical_trades.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/za/co/varsitycollege/opsc7312_poe_tactical_trades/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package za.co.varsitycollege.opsc7312_poe_tactical_trades.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
