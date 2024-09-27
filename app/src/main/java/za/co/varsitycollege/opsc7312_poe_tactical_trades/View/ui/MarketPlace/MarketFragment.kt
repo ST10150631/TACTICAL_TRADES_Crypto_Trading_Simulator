60a0d4f2e53d3633dc8367b9e02285412b17755e
Mikayle Coetzee <ST10023767@vcconnect.edu.za>
2024-09-27 16:03:16 +0200

    Updated Profile View in the Trade

--- app/src/main/java/za/co/varsitycollege/opsc7312_poe_tactical_trades/View/ui/MarketPlace/MarketFragment.kt
+++ app/src/main/java/za/co/varsitycollege/opsc7312_poe_tactical_trades/View/ui/MarketPlace/MarketFragment.kt
@@ -6,15 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import za.co.varsitycollege.opsc7312_poe_tactical_trades.Controller.FirebaseHelper
 import za.co.varsitycollege.opsc7312_poe_tactical_trades.R
 import za.co.varsitycollege.opsc7312_poe_tactical_trades.View.MainActivity
 import za.co.varsitycollege.opsc7312_poe_tactical_trades.View.ui.settings.SettingsFragment
+import za.co.varsitycollege.opsc7312_poe_tactical_trades.databinding.FragmentHomeBinding
 
 class MarketFragment : Fragment() {
     private lateinit var settingsButton: ImageButton
+    private val auth: FirebaseAuth by lazy { FirebaseHelper.firebaseAuth }
 
     companion object {
         fun newInstance() = MarketFragment()
@@ -35,8 +46,65 @@ class MarketFragment : Fragment() {
             }
         }
 
+        loadProfilePicture()
+        loadTotalBalance()
+
         return rootView
     }
+
+    private fun loadTotalBalance() {
+        FirebaseHelper.getTotalBalance(FirebaseHelper.firebaseAuth.currentUser?.uid ?: "") { balance, error ->
+            if (error != null) {
+                Toast.makeText(context, "Error fetching balance: $error", Toast.LENGTH_LONG).show()
+                val formattedBalance = "$0.00"
+
+                val txtBalance = view?.findViewById<TextView>(R.id.TxtBalance)
+                txtBalance?.text = formattedBalance
+
+            } else {
+                balance?.let {
+                    val formattedBalance = String.format("$%,.2f", it)
+
+                    val txtBalance = view?.findViewById<TextView>(R.id.TxtBalance)
+                    txtBalance?.text = formattedBalance
+
+                } ?: run {
+                    Toast.makeText(context, "Balance is null", Toast.LENGTH_SHORT).show()
+                    val formattedBalance = "$0.00"
+
+                    val txtBalance = view?.findViewById<TextView>(R.id.TxtBalance)
+                    txtBalance?.text = formattedBalance
+                }
+            }
+        }
+    }
+
+
+    private fun loadProfilePicture() {
+        val user = auth.currentUser
+        user?.let {
+            FirebaseHelper.getProfilePictureUrl(it.uid) { url, message ->
+
+                val imageView = view?.findViewById<ImageView>(R.id.myImageView)
+
+                if (imageView != null) {
+                    if (url != null) {
+                        Glide.with(this)
+                            .load(url)
+                            .apply(RequestOptions().transform(RoundedCorners(16)))
+                            .into(imageView)
+                    } else {
+                        Toast.makeText(requireContext(), "Failed to load profile picture: $message", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Image view is not available", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } ?: run {
+            Toast.makeText(requireContext(), "User not authenticated", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
