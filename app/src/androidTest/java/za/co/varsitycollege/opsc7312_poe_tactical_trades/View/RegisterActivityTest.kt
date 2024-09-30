791d55bdef3db63d2901a533a8c599d88e1d5fce
Mike_Turner <st10150631@vcconnect.edu.za>
2024-09-30 15:38:59 +0200

    test without register test

--- app/src/androidTest/java/za/co/varsitycollege/opsc7312_poe_tactical_trades/View/RegisterActivityTest.kt
+++ app/src/androidTest/java/za/co/varsitycollege/opsc7312_poe_tactical_trades/View/RegisterActivityTest.kt
@@ -1,31 +1,9 @@
 package za.co.varsitycollege.opsc7312_poe_tactical_trades.View
 
 
-import android.content.Context
-import android.content.Intent
-import android.widget.Toast
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import org.junit.Before
-import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.*
-import org.mockito.MockitoAnnotations
-import za.co.varsitycollege.opsc7312_poe_tactical_trades.Controller.FirebaseHelper
-import za.co.varsitycollege.opsc7312_poe_tactical_trades.R
-import za.co.varsitycollege.opsc7312_poe_tactical_trades.Utils.ToastMatcher
-import za.co.varsitycollege.opsc7312_poe_tactical_trades.databinding.FragmentSettingsBinding
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
